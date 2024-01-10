61acf5b44dd7a3eeca381e44fc84e7177bf296d5
Chandan2812 <chandanpms@gmail.com>
2024-01-10 13:56:44 +0530

    updated

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 
 
